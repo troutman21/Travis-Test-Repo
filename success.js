6dabbad1b7fb4810e3ce1972a9eb26229cde2fdf
Phillip Troutman <philliptroutman21@gmail.com>
2019-06-21 09:56:07 -0400

    changed font

--- success.js
+++ success.js
@@ -1,11 +1,15 @@
 const figlet = require('figlet');
 
 // change the first argument of the call to figlet to have it print on success
-figlet('Cohort 12 is awesome!!!', (err, data) => {
-  if (err) {
-    console.log('Something went wrong...');
-    console.dir(err);
-    return;
+figlet.text(
+  'Cohort 12 is awesome!!!',
+  { horizontalLayout: 'full' },
+  (err, data) => {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data);
   }
-  console.log(data);
-});
+);
