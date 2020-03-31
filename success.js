af92483d705ffa6bfcc3db0855eedd6d68a18c64
Phillip Troutman <philliptroutman21@gmail.com>
2020-03-31 16:46:46 -0400

    changed to cohort 34

--- success.js
+++ success.js
@@ -2,7 +2,7 @@ const figlet = require('figlet');
 
 // change the first argument of the call to figlet to have it print on success
 figlet.text(
-  'Cohort 12 is awesome!!!',
+  'Cohort 34 is awesome!!!',
   { horizontalLayout: 'full' },
   (err, data) => {
     if (err) {
