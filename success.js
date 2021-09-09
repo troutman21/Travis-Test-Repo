b176db604c81730c47f4ef6b2d8f4634dcb71168
Reid Klarsfeld <reidklarsfeld@Reids-MacBook-Air.local>
2021-09-09 11:22:18 -0700

    fix cohort

--- success.js
+++ success.js
@@ -2,7 +2,7 @@ const figlet = require('figlet');
 
 // change the first argument of the call to figlet to have it print on success
 figlet.text(
-  'Cohort 2 is awesome!!!',
+  'Cohort 3 is awesome!!!',
   { horizontalLayout: 'full' },
   (err, data) => {
     if (err) {
